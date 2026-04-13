32246541c1104ee4e5c03aa4f484322eb90f3b27
RamizUddin404 <ramizuddin404@gmail.com>
2026-04-13 22:08:04 +0600

    Add Capacitor for Android support

---
+++ android/app/src/main/java/com/example/jarvis/MainActivity.java
@@ -0,0 +1,5 @@
+package com.example.jarvis;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.example.jarvis',
+  appName: 'jarvis-ai-assistant',
+  webDir: 'dist'
+};
+
+export default config;
